d38c4bcd366534f14bd5151c09fcc5a6ac28a8f2
Emmanuel BIZIMANA <emmabnatha7@gmail.com>
2025-03-02 23:26:52 +0200

    access

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-@permission_required('bookshelf.can_edit', raise_exception=True)
+@permission_required('advanced_features_and_security.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('advanced_features_and_security.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        Book.objects.create(title=title, author=author)
+        return redirect('book_list')
+    return render(request, 'bookshelf/create_book.html')
+
+@permission_required('advanced_features_and_security.can_edit', raise_exception=True)
 def edit_book(request, book_id):
-    # Edit book logic
-    pass
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.save()
+        return redirect('book_list')
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+@permission_required('advanced_features_and_security.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    book.delete()
+    return redirect('book_list')
